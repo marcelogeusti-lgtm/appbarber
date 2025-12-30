7459d17befc647244bb2af6255835da890870bf2
marcelogeusti-lgtm <marcelogeusti@gmail.com>
2025-12-30 19:06:43 -0300

    Fix: Restaurada diretiva 'use client' e imports no Layout

--- client/app/dashboard/layout.js
+++ client/app/dashboard/layout.js
@@ -1,3 +1,6 @@
+'use client';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import {
     Calendar, History, PieChart, BadgeDollarSign, Users2,
