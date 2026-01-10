248d069b4e919fd4d79fa4719f651812b819e5b5
marcelogeusti-lgtm <marcelogeusti@gmail.com>
2026-01-10 01:06:28 -0300

    fix: use correct auth middleware function in communication routes

--- server/src/routes/communication.routes.js
+++ server/src/routes/communication.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const communicationController = require('../controllers/communication.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/status', authMiddleware, communicationController.getStatus);
+router.get('/status', authMiddleware.protect, communicationController.getStatus);
 // router.post('/send', authMiddleware, communicationController.sendManualMessage);
 
 module.exports = router;
