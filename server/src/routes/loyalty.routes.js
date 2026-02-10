5a9f441bc66a51aed81fe6578f12f85971b5a881
marcelogeusti-lgtm <marcelogeusti@gmail.com>
2026-02-10 18:54:37 -0300

    \fix: Correct middleware usage in loyalty and review routes to prevent crash"

--- server/src/routes/loyalty.routes.js
+++ server/src/routes/loyalty.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const loyaltyController = require('../controllers/loyalty.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/', authMiddleware, loyaltyController.getLoyaltySettings);
-router.put('/', authMiddleware, loyaltyController.updateLoyaltySettings);
+router.get('/', authMiddleware.protect, loyaltyController.getLoyaltySettings);
+router.put('/', authMiddleware.protect, loyaltyController.updateLoyaltySettings);
 
 module.exports = router;
--- server/src/routes/review.routes.js
+++ server/src/routes/review.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const reviewController = require('../controllers/review.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.post('/', authMiddleware, reviewController.createReview);
+router.post('/', authMiddleware.protect, reviewController.createReview);
 router.get('/', reviewController.getReviews); // Public or Auth? Usually public or mixed. Controller checks barbershopId.
 
 module.exports = router;
